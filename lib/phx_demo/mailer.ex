9a1856a3a9ceba7070a74a9d35676ec452aca283
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2024-05-21 22:17:12 +0200

    Initial commit

---
+++ lib/phx_demo/mailer.ex
@@ -0,0 +1,3 @@
+defmodule PhxDemo.Mailer do
+  use Swoosh.Mailer, otp_app: :phx_demo
+end
---
+++ lib/phx_demo_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule PhxDemoWeb.PageController do
+  use PhxDemoWeb, :controller
+
+  def home(conn, _params) do
+    # The home page is often custom made,
+    # so skip the default app layout.
+    render(conn, :home, layout: false)
+  end
+end
